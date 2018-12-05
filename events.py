58fa36bb11accabbaf076d29c7847da92d3a99c4
wowpin <30692035+wowpin@users.noreply.github.com>
2018-12-05 09:32:25 +0000

    Initial Commit

---
+++ events.py
@@ -0,0 +1,278 @@
+from functions import str2bool
+from functions import getFreeKey
+import time
+
+'''
+## Function template
+def (etarget, ebody, players, npcs, items, env):
+    players[etarget]
+'''
+
+def setPlayerCanGo(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	if etarget in players:
+		players[etarget]['canGo'] = int(ebody)
+
+def setPlayerCanLook(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	if etarget in players:
+		players[etarget]['canLook'] = int(ebody)
+
+def setPlayerCanSay(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	if etarget in players:
+		players[etarget]['canSay'] = int(ebody)
+
+def setPlayerCanAttack(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	if etarget in players:
+		players[etarget]['canAttack'] = int(ebody)
+
+def setPlayerCanDirectMessage(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	if etarget in players:
+		players[etarget]['canDirectMessage'] = int(ebody)
+
+def setPlayerPrefix(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	if etarget in players:
+		players[etarget]['prefix'] = str(ebody)
+
+def setPlayerName(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['name'] = str(ebody)
+
+def setPlayerRoom(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['name'] = str(ebody)
+
+def setPlayerLvl(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['lvl'] = int(ebody)
+
+def modPlayerLvl(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['lvl'] += int(ebody)
+
+def setPlayerExp(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['exp'] = int(ebody)
+
+def modPlayerExp(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['exp'] += int(ebody)
+
+def setPlayerStr(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['str'] = int(ebody)
+
+def modPlayerStr(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['str'] += int(ebody)
+
+def setPlayerPer(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['per'] = int(ebody)
+
+def modPlayerPer(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['per'] += int(ebody)
+
+def setPlayerEndu(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['endu'] = int(ebody)
+
+def modPlayerEndu(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+    players[etarget]['endu'] += int(ebody)
+
+def setPlayerCha(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['cha'] = int(ebody)
+
+def modPlayerCha(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['cha'] += int(ebody)
+
+def setPlayerInt(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['inte'] = int(ebody)
+
+def modPlayerInt(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['inte'] += int(ebody)
+
+def setPlayerAgi(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['agi'] = int(ebody)
+
+def modPlayerAgi(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['agi'] += int(ebody)
+
+def setPlayerLuc(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['luc'] = int(ebody)
+
+def modPlayerLuc(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['luc'] += int(ebody)
+
+def setPlayerCred(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['cred'] = int(ebody)
+
+def modPlayerCred(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['cred'] += int(ebody)
+
+def setPlayerInv(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['inv'] = str(ebody)
+
+def setAuthenticated(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['authenticated'] = str2bool(ebody)
+
+def setPlayerClo_head(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_larm(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_rarm(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_lhand(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_rhand(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_chest(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_lleg(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_rleg(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerClo_feet(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_head(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_larm(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_rarm(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_lhand(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_rhand(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_chest(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_lleg(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_rleg(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerImp_feet(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+
+def setPlayerHp(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['hp'] = int(ebody)
+
+def modPlayerHp(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['hp'] += int(ebody)
+
+def setPlayerCharge(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['charge'] = int(ebody)
+
+def modPlayerCharge(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['charge'] += int(ebody)
+
+def setPlayerIsInCombat(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['isInCombat'] += int(ebody)
+
+def setPlayerLastCombatAction(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['lastCombatAction'] = int(ebody)
+
+def modPlayerLastCombatAction(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['lastCombatAction'] += int(ebody)
+
+def setPlayerIsAttackable(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['isAttackable'] = int(ebody)
+
+def setPlayerLastRoom(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['lastRoom'] = str(ebody)
+
+def setPlayerCorpseTTL(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['corpseTTL'] = int(ebody)
+
+def modPlayerCorpseTTL(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	players[etarget]['corpseTTL'] += int(ebody)
+
+def spawnItem(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+	body = ebody.split(';')
+	items[getFreeKey(items, 200000)] = { 'id': int(body[0]), 'room': body[1], 'whenDropped': int(time.time()), 'lifespan': int(body[2]), 'owner': int(body[3])}
+
+def spawnNPC(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+	body = ebody.split(';')
+	# print(npcsDB)
+	# print(body)
+	npcs[getFreeKey(npcs, 90000)] = { 'name': npcsDB[int(body[0])]['name'], 'room': body[1], 'lvl': npcsDB[int(body[0])]['lvl'], 'exp': npcsDB[int(body[0])]['exp'], 'str': npcsDB[int(body[0])]['str'], 'per': npcsDB[int(body[0])]['per'], 'endu': npcsDB[int(body[0])]['endu'], 'cha': npcsDB[int(body[0])]['cha'], 'inte': npcsDB[int(body[0])]['inte'], 'agi': npcsDB[int(body[0])]['agi'], 'luc': npcsDB[int(body[0])]['luc'], 'cred': npcsDB[int(body[0])]['cred'], 'inv': npcsDB[int(body[0])]['inv'], 'isAttackable': int(body[2]), 'isStealable': int(body[3]), 'isKillable': int(body[4]), 'isAggressive': int(body[5]), 'clo_head': npcsDB[int(body[0])]['clo_head'], 'clo_larm': npcsDB[int(body[0])]['clo_larm'], 'clo_rarm': npcsDB[int(body[0])]['clo_rarm'], 'clo_lhand': npcsDB[int(body[0])]['clo_lhand'], 'clo_rhand': npcsDB[int(body[0])]['clo_rhand'], 'clo_chest': npcsDB[int(body[0])]['clo_chest'], 'clo_lleg': npcsDB[int(body[0])]['clo_lleg'], 'clo_rleg': npcsDB[int(body[0])]['clo_rleg'], 'clo_feet': npcsDB[int(body[0])]['clo_feet'], 'imp_head': npcsDB[int(body[0])]['imp_head'], 'imp_larm': npcsDB[int(body[0])]['imp_larm'], 'imp_rarm': npcsDB[int(body[0])]['imp_rarm'], 'imp_lhand': npcsDB[int(body[0])]['imp_lhand'], 'imp_rhand': npcsDB[int(body[0])]['imp_rhand'], 'imp_chest': npcsDB[int(body[0])]['imp_chest'], 'imp_lleg': npcsDB[int(body[0])]['imp_lleg'], 'imp_rleg': npcsDB[int(body[0])]['imp_rleg'], 'imp_feet': npcsDB[int(body[0])]['imp_feet'], 'hp': npcsDB[int(body[0])]['hp'], 'charge': npcsDB[int(body[0])]['charge'], 'corpseTTL': int(body[6]), 'respawn': int(body[7]), 'vocabulary': npcsDB[int(body[0])]['vocabulary'], 'talkDelay': npcsDB[int(body[0])]['talkDelay'], 'lookDescription': npcsDB[int(body[0])]['lookDescription'], 'timeTalked': 0, 'isInCombat': 0, 'lastCombatAction': 0, 'lastRoom': None, 'whenDied': None, 'randomizer': 0, 'randomFactor': npcsDB[int(body[0])]['randomFactor'], 'lastSaid': 0 }
+
+def spawnActor(etarget, ebody, players, npcs, items, env, npcsDB, envDB):
+	tempVar = 0
+	body = ebody.split(';')
+	env[int(body[0])] = { 'name': envDB[int(body[0])]['name'], 'vocabulary': envDB[int(body[0])]['vocabulary'], 'talkDelay': envDB[int(body[0])]['talkDelay'], 'randomFactor': envDB[int(body[0])]['randomFactor'], 'room': body[1], 'randomizer': envDB[int(body[0])]['randomizer'], 'timeTalked': envDB[int(body[0])]['timeTalked'], 'lastSaid': envDB[int(body[0])]['lastSaid'] }
+
+# Function for evaluating an Event
+
+def evaluateEvent(etarget, etype, ebody, players, npcs, items, env, npcsDB, envDB):
+    switcher = {
+        "setPlayerCanGo": setPlayerCanGo, #
+        "setPlayerCanLook": setPlayerCanLook, #
+        "setPlayerCanSay": setPlayerCanSay, #
+        "setPlayerCanAttack": setPlayerCanAttack, #
+        "setPlayerCanDirectMessage": setPlayerCanDirectMessage, #
+        "setPlayerName": setPlayerName, #
+        "setPlayerPrefix": setPlayerPrefix, #
+        "setPlayerRoom": setPlayerRoom, #
+        "setPlayerLvl": setPlayerLvl, #
+        "modPlayerLvl": modPlayerLvl, #
+        "setPlayerExp": setPlayerExp, #
+        "modPlayerExp": modPlayerExp, #
+        "setPlayerStr": setPlayerStr, #
+        "modPlayerStr": modPlayerStr, #
+        "setPlayerPer": setPlayerPer, #
+        "modPlayerPer": modPlayerPer, #
+        "setPlayerEndu": setPlayerEndu, #
+        "modPlayerEndu": modPlayerEndu, #
+        "setPlayerCha": setPlayerCha, #
+        "modPlayerCha": modPlayerCha, #
+        "setPlayerInt": setPlayerInt, #
+        "modPlayerInt": modPlayerInt, #
+        "setPlayerAgi": setPlayerAgi, #
+        "modPlayerAgi": modPlayerAgi, #
+        "setPlayerLuc": setPlayerLuc, #
+        "modPlayerLuc": modPlayerLuc, #
+        "setPlayerCred": setPlayerCred, #
+        "modPlayerCred": modPlayerCred, #
+        "setPlayerInv": setPlayerInv, #
+        "setAuthenticated": setAuthenticated, #
+        "setPlayerClo_head": setPlayerClo_head, #
+        "setPlayerClo_larm": setPlayerClo_larm, #
+        "setPlayerClo_rarm": setPlayerClo_rarm, #
+        "setPlayerClo_lhand": setPlayerClo_lhand, #
+        "setPlayerClo_rhand": setPlayerClo_rhand, #
+        "setPlayerClo_chest": setPlayerClo_chest, #
+        "setPlayerClo_lleg": setPlayerClo_lleg, #
+        "setPlayerClo_rleg": setPlayerClo_rleg, #
+        "setPlayerClo_feet": setPlayerClo_feet, #
+        "setPlayerImp_head": setPlayerImp_head, #
+        "setPlayerImp_larm": setPlayerImp_larm, #
+        "setPlayerImp_rarm": setPlayerImp_rarm, #
+        "setPlayerImp_lhand": setPlayerImp_lhand, #
+        "setPlayerImp_rhand": setPlayerImp_rhand, #
+        "setPlayerImp_chest": setPlayerImp_chest, #
+        "setPlayerImp_lleg": setPlayerImp_lleg, #
+        "setPlayerImp_rleg": setPlayerImp_rleg, #
+        "setPlayerImp_feet": setPlayerImp_feet, #
+        "setPlayerHp": setPlayerHp, #
+        "modPlayerHp": modPlayerHp, #
+        "setPlayerCharge": setPlayerCharge, #
+        "modPlayerCharge": modPlayerCharge, #
+        "setPlayerIsInCombat": setPlayerIsInCombat, #
+        "setPlayerLastCombatAction": setPlayerLastCombatAction, #
+        "modPlayerLastCombatAction": modPlayerLastCombatAction, #
+        "setPlayerIsAttackable": setPlayerIsAttackable, #
+        "setPlayerLastRoom": setPlayerLastRoom, #
+        "setPlayerCorpseTTL": setPlayerCorpseTTL, #
+        "modPlayerCorpseTTL": modPlayerCorpseTTL, #
+		"spawnItem": spawnItem,
+		"spawnNPC": spawnNPC,
+		"spawnActor": spawnActor,
+        }
+    switcher[etype](etarget, ebody, players, npcs, items, env, npcsDB, envDB)
---
+++ functions.py
@@ -0,0 +1,204 @@
+import time
+import os
+import commentjson
+import errno
+from copy import deepcopy
+
+# Function to silently remove file 
+def silentRemove(filename):
+    try:
+        os.remove(filename)
+    except OSError as e: # this would be "except OSError, e:" before Python 2.6
+        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
+            raise # re-raise exception if a different error occurred
+
+# Function to load all registered players from JSON files
+def loadPlayersDB(location = "./players", forceLowercase = True):
+	DB = {}
+	playerFiles = [i for i in os.listdir(location) if os.path.splitext(i)[1] == ".player"]
+	for f in playerFiles:
+		with open(os.path.join(location,f)) as file_object:
+			#playersDB[f] = file_object.read()
+			DB[f] = commentjson.load(file_object)
+
+	if forceLowercase is True:
+		out = {}
+		for key, value in DB.items():
+			out[key.lower()] = value
+
+		return(out)
+	else:
+		return(DB)
+
+	#for i in playersDB:
+		#print(i, playersDB[i])
+
+# Function used for loggin messages to stdout and a disk file
+def log(content, type):
+    logfile = 'dum.log'
+    print(str(time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + " [" + type + "] " + content))
+    if os.path.exists(logfile):
+        log = open(logfile, 'a')
+    else:
+        log = open(logfile, 'w')
+    log.write(str(time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + " [" + type + "] " + content) + '\n')
+    log.close()
+
+# Function for returning a first available key value for appending a new element to a dictionary
+'''
+def getFreeKey(itemsDict):
+    try:
+        for x in range(0, len(itemsDict) + 1):
+            if len(itemsDict[x]) > 0:
+                pass
+    except Exception:
+        pass
+    return(x)
+'''
+
+def getFreeKey(itemsDict, start = None):
+    if start is None:
+        try:
+            for x in range(0, len(itemsDict) + 1):
+                if len(itemsDict[x]) > 0:
+                    pass
+        except Exception:
+            pass
+        return(x)
+    else:
+        found = False
+        while found is False:
+            if start in itemsDict:
+                start += 1
+            else:
+                found = True
+        return(start)
+
+# Function for adding events to event scheduler
+def addToScheduler(eventID, targetID, scheduler, database):
+    for item in database:
+        if int(item[0]) == eventID:
+            scheduler[getFreeKey(scheduler)] = { 'time': int(time.time() + int(item[1])), 'target': int(targetID), 'type': item[2], 'body': item[3] }
+
+def loadPlayer(name, db):
+	try:
+		#with open(path + name + ".player", "r") as read_file:
+			#dict = commentjson.load(read_file)
+			#return(dict)
+		return(db[name.lower() + ".player"])
+	except Exception:
+		pass
+
+def savePlayer(player, masterDB, path = "./players" + "/"):
+	#print(path)
+	DB = loadPlayersDB(forceLowercase = False)
+	for p in DB:
+		if (player['name'] + ".player").lower() == p.lower():
+			#print("found the file")
+			#print(p)
+			with open(path + p, "r") as read_file:
+				temp = commentjson.load(read_file)
+			#print(temp)
+			silentRemove(path + player['name'] + ".player")
+			#print("removed file")
+			newPlayer = deepcopy(temp)
+			#print(newPlayer)
+			newPlayer['pwd'] = temp['pwd']
+			for key in newPlayer:
+				if key != "pwd":
+					# print(key)
+					newPlayer[key] = player[key]
+			#print(newPlayer)
+			#print("Saving player state")
+			with open(path + player['name'] + ".player", 'w') as fp:
+				commentjson.dump(newPlayer, fp)
+			#print("Updating playerd DB")
+			masterDB = loadPlayersDB()
+			#print(masterDB)
+	'''
+	if name.lower() + ".player" is in playersDB:
+		tempVar=0
+	silentRemove(path + name + ".player")
+	with open(path + name + ".player", 'w') as fp:
+		commentjson.dump(dict, fp)
+	db = loadPlayersDB()
+	'''
+
+
+# State Save Function
+def saveState(player, masterDB):
+	tempVar = 0
+	savePlayer(player, masterDB)
+	#masterDB = loadPlayersDB()
+
+'''
+# State save function
+def saveState(p, connector):
+            cursor = connector.cursor()
+            cursor.execute("UPDATE tbl_Players SET room = %s WHERE name = %s", [p["room"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET lvl = %s WHERE name = %s", [p["lvl"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET exp = %s WHERE name = %s", [p["exp"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET str = %s WHERE name = %s", [p["str"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET per = %s WHERE name = %s", [p["per"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET endu = %s WHERE name = %s", [p["endu"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET cha = %s WHERE name = %s", [p["cha"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET inte = %s WHERE name = %s", [p["int"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET agi = %s WHERE name = %s", [p["agi"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET luc = %s WHERE name = %s", [p["luc"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET cred = %s WHERE name = %s", [p["cred"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET inv = %s WHERE name = %s", [str(",".join(p["inv"])), p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_head = %s WHERE name = %s", [p["clo_head"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_larm = %s WHERE name = %s", [p["clo_larm"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_rarm = %s WHERE name = %s", [p["clo_rarm"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_lhand = %s WHERE name = %s", [p["clo_lhand"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_rhand = %s WHERE name = %s", [p["clo_rhand"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_chest = %s WHERE name = %s", [p["clo_chest"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_lleg = %s WHERE name = %s", [p["clo_lleg"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_rleg = %s WHERE name = %s", [p["clo_rleg"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET clo_feet = %s WHERE name = %s", [p["clo_feet"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_head = %s WHERE name = %s", [p["imp_head"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_larm = %s WHERE name = %s", [p["imp_larm"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_rarm = %s WHERE name = %s", [p["imp_rarm"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_lhand = %s WHERE name = %s", [p["imp_lhand"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_rhand = %s WHERE name = %s", [p["imp_rhand"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_chest = %s WHERE name = %s", [p["imp_chest"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_lleg = %s WHERE name = %s", [p["imp_lleg"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_rleg = %s WHERE name = %s", [p["imp_rleg"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET imp_feet = %s WHERE name = %s", [p["imp_feet"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET hp = %s WHERE name = %s", [p["hp"], p["name"]])
+            connector.commit()
+            cursor.execute("UPDATE tbl_Players SET charge = %s WHERE name = %s", [p["charge"], p["name"]])
+            connector.commit()
+'''
+def str2bool(v):
+  return v.lower() in ("yes", "true", "True", "t", "1")
