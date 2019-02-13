ec67c03aca6d3558dd20e084e5840963a07095a9
wowpin <bartek.radwanski@gmail.com>
2019-02-13 10:20:44 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+yes | sudo pip3 install commentjson
+sudo apt-get -y install git-core
+git clone https://github.com/wowpin/dumserver.git
