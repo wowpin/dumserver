86d29c0a7672bc7af02473e59053222ad1c46cb7
Chris Byrd <chrisbyrd@bwicompanies.com>
2019-10-09 14:38:40 -0500

    .

--- lib/python3.7/site-packages/commentjson/tests/test_json/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import commentjson
-
-
-class CommentJsonTest(unittest.TestCase):
-    json = commentjson
-    loads = staticmethod(commentjson.loads)
-    dumps = staticmethod(commentjson.dumps)
