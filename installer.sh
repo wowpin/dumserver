83a8d8678a5f69dbf4e92f7ba5b20938e5417a6d
wowpin <bartek.radwanski@gmail.com>
2019-12-23 13:13:31 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,15 +5,17 @@ yes | sudo pip3 install websocket-client
 sudo apt-get -y install git-core
 
 sudo apt-get -y install nodejs
-cd webclient
-sudo chmod +x install-npm.sh
-sudo ./install-npm.sh
-cd ..
-sudo npm install -g forever
 
 git clone https://github.com/wowpin/dumserver.git
 
 cd dumserver
+
+cd webclient
+sudo ./install-npm.sh
+cd ..
+
+sudo npm install -g forever
+
 sudo pip3 install -r requirements.txt
 cd ..
 
