829786dd4f46750bbe4e1940cf161edfe97d62c7
wowpin <bartek.radwanski@gmail.com>
2019-12-30 13:08:23 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,23 +1,33 @@
+# Update and Upgrade your distro before installing anything
+sudo apt update && yes | sudo apt upgrade
+
+# Install Python3 and python-related packages
 sudo apt-get -y install python3
 sudo apt-get -y install python3-pip
 yes | sudo pip3 install commentjson
 yes | sudo pip3 install websocket-client
 sudo apt-get -y install git-core
 
+# Install Webclient pre-requisites - Node.js and NPM 
 sudo apt-get -y install nodejs
 curl -L https://npmjs.org/install.sh | sudo sh
 
+# Install the server nanny used for restarting the Webclient should it terminate
 sudo npm install -g forever
 
+# Clone dumserver
 git clone https://github.com/wowpin/dumserver.git
 
 cd dumserver
 
+# Install required Python modules
 sudo pip3 install -r requirements.txt
 cd ..
 
+# Install required Node libraries
 cd dumserver/webclient
 sudo npm install
 cd ..
 
+# Run DUM configuration
 sudo python3 setup.py
