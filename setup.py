9cddbe27ce5ab83ec84b5fba7a8c5944c8cf2076
wowpin <bartek.radwanski@gmail.com>
2019-12-30 14:59:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+__filename__ = "setup.py"
+__author__ = "Bartek Radwanski"
+__credits__ = "Bartek Radwanski"
+__license__ = "MIT"
+__version__ = "0.7.1"
+__maintainer__ = "Bartek Radwanski"
+__email__ = "bartek.radwanski@gmail.com"
+__status__ = "Stable"
 
 import os
 from requests import get
+
 def yes_or_no(question):
     reply = " "
     while reply[0] is not 'y' or reply[0] is not 'n':
@@ -11,29 +20,37 @@ def yes_or_no(question):
             return False
         else:
             return yes_or_no("Uhhhh... please enter ")
-        
+
 print('\n\n\n')
+
 with open('/tmp/dum.home', 'r') as file:
     dumhome = str(file.read().replace('\n', ''))
 file.close()
+
 print('Dum installed in: ' + dumhome)
 print('\n')
+
 ip = get('https://api.ipify.org').text
 print('Following Public IP address has been detected:', ip)
 if yes_or_no('Would you like to use it for DUM Webclient configuration?'):
     pass
 else:
     ip = str(input("Please input Public IP:")).strip()
+    
 # Read in the webclient config file
 with open(str(dumhome + '/webclient/config/default.js'), 'r') as file :
   filedata = file.read()
+
 # Update the config file
 filedata = filedata.replace('PUBLIC_IP', str(ip))
+
 # Write the file out again
 with open(str(dumhome + '/webclient/config/default.js'), 'w') as file:
   file.write(filedata)
+
 if not os.path.exists(dumhome + '/setup.completed'):
     open(str(dumhome + '/setup.completed'), 'a').close()
+
 print('\ndumserver configuration has been completed. You can start using the sever with the help of following commands:')
 print('./server-start.sh    -    Boot up an instance of dumserver. Once up and running, server can be accessed on http://<Your public IP>')
 print('./server-status.sh   -    Check the status of dumserver components')
