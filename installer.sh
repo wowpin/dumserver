2e0e97bd931cf36aef513ade89d8b1362a23a5b5
wowpin <bartek.radwanski@gmail.com>
2019-12-30 09:01:11 +0100

    Update installer.sh
    Fixing the NPM version

--- installer.sh
+++ installer.sh
@@ -5,14 +5,13 @@ yes | sudo pip3 install websocket-client
 sudo apt-get -y install git-core
 
 sudo apt-get -y install nodejs
-sudo apt-get -y install npm
+curl -L https://npmjs.org/install.sh | sudo sh
+
 sudo npm install -g forever
 
 git clone https://github.com/wowpin/dumserver.git
 
 cd dumserver
-
-sudo npm install -g forever
 
 sudo pip3 install -r requirements.txt
 cd ..
